dfb91883496715e8767ed44fadcb1f7af66c12bd
Chris Flannery <chriswillsflannery@gmail.com>
2024-05-17 10:30:42 -0400

    minstack py

--- algos/minstack/minstack.py
+++ algos/minstack/minstack.py
@@ -1,6 +1,19 @@
-class Minstack {
-  def push:
-  def pop:
-  def top:
-  def minElement:
-}
+from typing import List
+class Minstack:
+  def __init__(self, stack: List[int], minElementStack: List[int]) -> None:
+    self.stack = stack
+    self.minElementStack = minElementStack
+
+  def push(self, el):
+    self.stack.append(el)
+    self.minElementStack.append(min(el, self.minElementStack[-1]))
+
+  def pop(self):
+    self.minElementStack.pop()
+    return self.stack.pop()
+    
+  def top(self):
+    return self.stack[-1]
+  
+  def minElement(self):
+    return self.minElementStack[-1]

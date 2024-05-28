2f371f088a5341f8fe3e31d0c028cddaaf8c244b
Chris Flannery <chriswillsflannery@gmail.com>
2024-05-28 10:04:50 -0400

    greatFrontend easy

---
+++ greatFrontend/clamp/clamp.go
@@ -0,0 +1,32 @@
+/*
+
+Implement a function clamp(number, lower, upper) to restrict a number within the inclusive lower and upper bounds.
+
+Arguments
+value (number): The number to clamp.
+lower (number): The lower bound.
+upper (number): The upper bound.
+Returns
+(number): Returns the clamped number.
+
+Examples
+// Within the bounds, return as-is.
+clamp(3, 0, 5); // => 3
+
+// Smaller than the lower bound.
+clamp(-10, -3, 5); // => -3
+
+// Bigger than the upper bound.
+clamp(10, -5, 5); // => 5
+
+*/
+
+func clamp(value int, lower int, upper int) int {
+	if value >= lower && value <= upper {
+		return value
+	} else if  value < lower {
+		return lower
+	} else {
+		return upper
+	}
+}

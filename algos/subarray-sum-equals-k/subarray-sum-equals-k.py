8c49cee2d488726b5f87d7a49ebe54eeaa592bbd
Chris Flannery <chriswillsflannery@gmail.com>
2024-09-14 14:57:27 -0400

    melt my brain

--- algos/subarray-sum-equals-k/subarray-sum-equals-k.py
+++ algos/subarray-sum-equals-k/subarray-sum-equals-k.py
@@ -27,6 +27,19 @@ class Solution:
 
         # make sure to include an empty prefixSum for 0
 
+        # [magic implicit 0 prefix here] [1,-1,1,1,1,1] k = 3
+        # for each value in the array, we say,
+        # take the sum up to this point. subtract k from it.
+        # we get a resulting value.
+        # we say, how many times have we seen this resulting value before?
+        # add that count to our result.
+        # then we make sure that currentSum gets incremented
+        # (we have now seen this currentSum 1 more time)
+
+        #clarifying q's
+        # why do we need the magic 0 again?
+
+
         res = 0
         currSum = 0
 
@@ -40,4 +53,62 @@ class Solution:
             prefixSums[currSum] = 1 + prefixSums.get(currSum, 0)
         return res
 
+    """
+    overall rationale can be summed up like:
+    we want to know how many different contiguous subarrays
+    can sum up to k.
 
+    we can get determine whether a subarray sums up to k
+    by taking the total sum up to that point, eg
+    [1,2,3,4,5] k = 12
+         ^   ^
+    here we want to know if 3,4,5 == k
+    so we take the total sum up to that point (15):
+    [1,3,6,10,15]
+
+    and we say: ok , if I take 15 - k, which is 3:
+    is there a total sum up to some point which === 3?
+    and more specifically (since there is the possibility
+    of having negative numbers,)
+    how many total sums up to some point === 3?
+
+    so we start off with an empty dict (account for magic 0)
+    { 0: 1 }
+    and an empty total sum: 0
+    and an empty res: 0 (this will track how many subarrays total k)
+
+    and we say: ok, first sum is 1. 1-k = -11.
+    Have we seen -11 as a sum before (is it a key in dict?)
+    no, it's not. ok, do nothing.
+    add this sum to our dict. 
+    dict is now { 0: 1, 1: 1 }
+
+    ok, next sum is 3. 3-k = -9.
+    Have we seen -9 as a sum before?
+    no, it's not. Ok, do nothing.
+    Add this sum to our dict.
+    dict is now { 0: 1, 1:1, 3:1 }
+
+    ok, next sum is 6. 6-k = -6
+    Have we seen -6 as a sum before?
+    no. do nothing.
+    Add this sum to our dict.
+    dict is now {0:1,1:1,3:1,6:1}
+
+    ok, next sum is 10. 10-k = -2
+    have we seen -2 as a sum before?
+    no. do nothing.
+    Add this sum to our dict.
+    dict is now {0:1,1:1,3:1,6:1,10:1}
+
+    ok, next sum is 15. 15-k = 3.
+    Have we seen 3 as a sum before?
+    Yes, we have, 1 time. Ok, add 1 to our res.
+    add this sum to our dict.
+     dict is now {0:1,1:1,3:1,6:1,10:1, 15:1}
+
+    now we've reached end of list.
+    res is 1.
+    Therefore, there is 1 contiguous subarray here
+    which == 12, that is, [3,4,5]
+    """

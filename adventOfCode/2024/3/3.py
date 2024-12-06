f55ce771223ad78dc3c86060d87d790e6fcd2b4c
Chris Flannery <chriswillsflannery@gmail.com>
2024-12-05 20:41:38 -0500

    advent day 3 part 2

--- adventOfCode/2024/3/3.py
+++ adventOfCode/2024/3/3.py
@@ -31,7 +31,40 @@ def sum_muls(filecontents):
     suma += prod
   return suma
 
+def sum_muls2(filecontents):
+    # match literal strings do() and dont()
+    do = r"do\(\)"
+    dont = r"don't\(\)"
+    # use capture groups to match mul(x,y) and extract x,y as group(1) and group(2)
+    mul = r"mul\((\d+),(\d+)\)"
+
+    total = 0
+    enabled = True
+    for x in re.finditer(f"{do}|{dont}|{mul}", filecontents):
+      if re.fullmatch(do, x.group()):
+        enabled = True
+      elif re.fullmatch(dont, x.group()):
+        enabled = False
+      elif enabled:
+        # don't understand enoughh yet about pattern matching to understand why this works
+        # where x.group = "mul(x,y)"
+        # why does x.group(1) == x
+        total += int(x.group(1)) * int(x.group(2))
+    return total
+
+
 if __name__ ==  "__main__":
   parsed_text = parse_text('3-input.txt')
   summer = sum_muls(parsed_text)
   print(summer)
+
+  summer2 = sum_muls2(parsed_text)
+  print(summer2)
+
+  ## part 2
+  """
+  we should parse out any instances of "do()" or "don't()"
+  
+  Define a regex pattern to match both mul(x,y), don't(), and do()
+pattern = r"mul\(\d+,\d+\)|don't\(\)|do\(\)"
+  """

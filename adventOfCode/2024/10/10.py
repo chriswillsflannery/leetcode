4e22e0e1822a88c29847430bc44c5324ad80ba91
Chris Flannery <chriswillsflannery@gmail.com>
2025-01-01 17:01:55 -0500

    reachable nines:

--- adventOfCode/2024/10/10.py
+++ adventOfCode/2024/10/10.py
@@ -1,25 +1,51 @@
+from collections import deque
+from typing import List
+
+
 def parse_text(filename):
   with open(filename, "r") as file:
     lines = []
     for line in file:
-      lines.append(list(line.strip()))
+      lines.append([int(char) for char in line.strip()])
   return lines
 
-def bfs_grid(grid: list(list(str))) -> int:
-  return 9
+def dfs_grid(grid: List[List[int]]) -> int:
+  ROWS = len(grid)
+  COLS = len(grid[0])
+  directions = [(0,1),(1,0),(0,-1),(-1,0)]
 
-# [
-#   ['8', '9', '0', '1', '0', '1', '2', '3'],
-#   ['7', '8', '1', '2', '1', '8', '7', '4'],
-#   ['8', '7', '4', '3', '0', '9', '6', '5'],
-#   ['9', '6', '5', '4', '9', '8', '7', '4'],
-#   ['4', '5', '6', '7', '8', '9', '0', '3'],
-#   ['3', '2', '0', '1', '9', '0', '1', '2'],
-#   ['0', '1', '3', '2', '9', '8', '0', '1'],
-#   ['1', '0', '4', '5', '6', '7', '3', '2']]
+  def dfs(r: int, c: int, visited: set, reachable_nines: set) -> None:
+    if grid[r][c] == 9:
+      reachable_nines.add((r,c))
+      return
+    
+    current_height = grid[r][c]
+
+    for dx, dy in directions:
+      r_offset,c_offset = r+dx, c+dy
+      if (0 <= r_offset < ROWS and
+          0 <= c_offset < COLS and
+          grid[r_offset][c_offset] == current_height + 1 and
+          (r_offset, c_offset) not in visited):
+        visited.add((r_offset,c_offset))
+        dfs(r_offset,c_offset,visited,reachable_nines)
+        #backtrack - remove from visited
+        visited.remove((r_offset, c_offset))
+  total_score = 0
+
+  #find all trailheads
+  for i in range(ROWS):
+    for j in range(COLS):
+      if grid[i][j] == 0:
+        # for each trailhead find reachable nines
+        reachable_nines = set()
+        dfs(i,j,{(i,j)},reachable_nines)
+        score = len(reachable_nines)
+        total_score += score
+  return total_score
 
 if __name__ == '__main__':
   parsed_text = parse_text('10-input.txt')
   print(parsed_text)
-  num_paths = bfs_grid(parsed_text)
+  num_paths = dfs_grid(parsed_text)
   print(num_paths)
